ad8b470e6f36be798ad3e2a1d9213e7988bbc837
jku20 <jku-ben20@stuy.edu>
2020-12-20 23:56:41 -0500

    updated homebase.h

--- homebase.h
+++ homebase.h
@@ -2,4 +2,8 @@
 #define HOMEBASE_H
 int run_cmd(char *cmd);
 void print_prompt();
+int norm_cmd(char *cmd);
+int redirect_file_descriptor(int original_fd, int new_fd);
+int redirected_out_cmd(char *cmd, char append);
+int redirected_in_cmd(char *cmd);
 #endif
